9ea5547b41f3159ec939d1a6429050dc69e5f1e9
Biplob Sarkar <bcsarkar02@gmail.com>
2018-06-26 16:04:09 +0600

    Workable menu and card added for main content

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule, MatIconModule, MatSidenavModule, MatToolbarModule, MatListModule, MatCardModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatCardModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatCardModule
+  ],
+  declarations: []
+})
+export class MaterialModule { }
